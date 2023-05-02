ed247b76928014a576d8feb27bf5aea61c0c47cd
ARcKP98 <pkartik4386@outlook.com>
2023-05-02 19:58:05 +0000

    Add Book model.

--- book_review/models.py
+++ book_review/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth import User
+
 
 # Create your models here.
+class Book(models.Model):
+    name = models.CharField(max_length=300, blank=False, unique=True)
+    author = models.CharField(max_length=300, blank=False, unique=False)
+    genre = models.ForeignKey(Genre, on_delete=models.PROTECT)
+    blurb = models.TextField()
+    slug = models.SlugField(max_length=200, unique=True)
+    approved = models.BooleanField(default=False)
+    like_count = models.ManyToManyField(User, blank=True)
+    uploaded_by = models.ForeignKey(User, on_delete=models.CASCADE)
