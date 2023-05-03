5033237bf3574e1a7c6e6dcebc311444edb4387f
ARcKP98 <pkartik4386@outlook.com>
2023-05-03 09:21:55 +0000

    Update Book model.

--- book_review/models.py
+++ book_review/models.py
@@ -1,14 +1,25 @@
 from django.db import models
 from django.contrib.auth import User
+from cloudinary.models import CloudinaryField
 
 
 # Create your models here.
 class Book(models.Model):
     name = models.CharField(max_length=300, blank=False, unique=True)
     author = models.CharField(max_length=300, blank=False, unique=False)
-    genre = models.ForeignKey(Genre, on_delete=models.PROTECT)
+    genre = models.ForeignKey(Genre, on_delete=models.PROTECT, related_name='book_genre')
     blurb = models.TextField()
     slug = models.SlugField(max_length=200, unique=True)
     approved = models.BooleanField(default=False)
     like_count = models.ManyToManyField(User, blank=True)
-    uploaded_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    uploaded_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='book_contributor')
+    image = CloudinaryField('image', default='Placeholder')
+
+    class Meta:
+        ordering = ['-created_on']
+
+        def number_of_likes(self):
+            return self.like_count.count()
+
+        def __str__(self):
+            return self.name
