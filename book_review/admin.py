42bcc47168e72eb0d8a81e8dd17b397ba4a105a6
ARcKP98 <pkartik4386@outlook.com>
2023-05-04 14:42:36 +0000

    Change the genre admin model.

--- book_review/admin.py
+++ book_review/admin.py
@@ -2,6 +2,12 @@ from django.contrib import admin
 from .models import Genre, Book, Review
 
 # Register your models here.
-admin.site.register(Genre)
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ['name']}
+    list_filter = ['name']
+
+
+
 admin.site.register(Book)
 admin.site.register(Review)
