c37b59b23514f6d2e6d0774c065979f22221729f
ARcKP98 <pkartik4386@outlook.com>
2023-05-09 15:31:10 +0000

    Fix view code.

--- book_review/views.py
+++ book_review/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 from django.views import generic
-from .models import Genre, Book, Review
+from .models import Book
 
 
 # Create your views here.
 class BookList(generic.ListView):
     model = Book
     queryset = Book.objects.filter(approved=True).order_by('?')
-    template = 'index.html'
+    template_name = 'index.html'
     paginate_by = 6
