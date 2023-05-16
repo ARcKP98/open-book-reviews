33ba73175d130e1deec77f77e50d3e20a3292708
ARcKP98 <pkartik4386@outlook.com>
2023-05-16 14:09:49 +0000

    Change the template to show genres.

--- book_review/views.py
+++ book_review/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
 from django.views import generic
-from .models import Book
+from .models import Book, Genre
 
 
 # Create your views here.
-class BookList(generic.ListView):
-    model = Book
-    queryset = Book.objects.filter(approved=True).order_by('?')
+# class BookList(generic.ListView):
+#     model = Book
+#     queryset = Book.objects.filter(approved=True).order_by('?')
+#     template_name = 'index.html'
+#     paginate_by = 6
+
+
+class GenreList(generic.ListView):
+    model = Genre
     template_name = 'index.html'
+    queryset = Genre.objects.order_by('name')
     paginate_by = 6
