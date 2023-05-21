ced2400d582ff6303ffe9ccd1c9a9eb1f1465ba8
ARcKP98 <pkartik4386@outlook.com>
2023-05-21 13:17:17 +0000

    Add book-detail view.

--- book_review/urls.py
+++ book_review/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 urlpatterns = [
     path('', views.GenreList.as_view(), name='home'),
     path("genre/<genre>/", views.ViewGenreTitles.as_view(), name="genre"),
+    path('<slug:slug>/', views.BookInfo.as_view(), name='book-info'),
 ]
--- book_review/views.py
+++ book_review/views.py
@@ -1,15 +1,9 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Book, Genre
 
 
 # Create your views here.
-# class BookList(generic.ListView):
-#     model = Book
-#     queryset = Book.objects.filter(approved=True).order_by('?')
-#     template_name = 'index.html'
-#     paginate_by = 6
-
 
 class GenreList(generic.ListView):
     model = Genre
@@ -19,12 +13,30 @@ class GenreList(generic.ListView):
 
 
 class ViewGenreTitles(generic.ListView):
-    template_name = 'genre.html'
+    template_name = 'books.html'
     context_object_name = 'genre_items'
 
     def get_queryset(self):
         content = {
             'genre': self.kwargs['genre'],
-            'books': Book.objects.filter(genre__slug=self.kwargs['genre']).filter(approved=True)
+            'books': Book.objects.filter
+            (genre__slug=self.kwargs['genre']).filter(approved=True)
         }
         return content
+
+
+class BookInfo(View):
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Book.objects.filter(approved=True)
+        book = get_object_or_404(queryset, slug=slug)
+        like = False
+        if book.like_count.filter(id=self.request.user.id).exists():
+            like = True
+        return render(
+            request,
+            'book-info.html',
+            {
+                'book': book,
+                'like': like,
+            }
+        )
