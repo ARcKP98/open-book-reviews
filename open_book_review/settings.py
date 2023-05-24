6854e630cec9c53d91baf95e006b0f1857a0e4e1
ARcKP98 <pkartik4386@outlook.com>
2023-05-24 08:25:02 +0000

    Change border colour. Remove host url.

--- open_book_review/settings.py
+++ open_book_review/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["open-book-review.herokuapp.com", "localhost", "8000-arckp98-openbookreviews-7lart36h7bv.ws-eu97.gitpod.io"]
+ALLOWED_HOSTS = ["open-book-review.herokuapp.com", "localhost"]
 
 
 # Application definition
