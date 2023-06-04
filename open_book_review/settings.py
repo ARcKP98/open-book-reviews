754751c6c8f293ecf32cbe989554bfceb31c0f4c
ARcKP98 <pkartik4386@outlook.com>
2023-06-04 12:34:39 +0000

    Fix the display of read-btn.

--- open_book_review/settings.py
+++ open_book_review/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["open-book-review.herokuapp.com", "localhost"]
+ALLOWED_HOSTS = ["open-book-review.herokuapp.com", "localhost", "8000-arckp98-openbookreviews-7lart36h7bv.ws-eu98.gitpod.io"]
 
 
 # Application definition
